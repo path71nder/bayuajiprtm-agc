c255653df8b27dacadbc5cac3f2260889ead8e9c
path71nder <sevillebayuaji@gmail.com>
2023-04-04 22:37:50 +0700

    menambahkan Example.cs

---
+++ Assets/Example.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Example : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int example1, example2;
+        example1 = 10;
+        example2 = 25;
+        Debug.Log(example1 + example2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
